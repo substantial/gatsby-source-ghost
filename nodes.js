b5a615d5e45bae7f753e0463857654bc290c2d69
Beau Dacious <dacious.beau@gmail.com>
2018-12-14 15:02:13 -0800

    use node references where applicable

--- nodes.js
+++ nodes.js
@@ -1,7 +1,8 @@
 const createNodeHelpers = require('gatsby-node-helpers').default;
 
 const {
-    createNodeFactory
+    createNodeFactory,
+    generateNodeId
 } = createNodeHelpers({
     typePrefix: 'Ghost'
 });
@@ -11,7 +12,68 @@ const PAGE = 'Page';
 const TAG = 'Tag';
 const AUTHOR = 'Author';
 
-module.exports.PostNode = createNodeFactory(POST);
-module.exports.PageNode = createNodeFactory(PAGE);
+const replacePropWithNodeRef = (node, prop, refId) => {
+    node[`${prop}___NODE`] =
+        typeof refId === 'function' ? refId(node[prop]) : refId;
+    delete node[prop];
+};
+
+module.exports.PostNode = createNodeFactory(POST, (node) => {
+    if (node.primary_tag) {
+        replacePropWithNodeRef(
+            node,
+            'primary_tag',
+            generateNodeId(TAG, node.primary_tag.id)
+        );
+    }
+    if (node.primary_author) {
+        replacePropWithNodeRef(
+            node,
+            'primary_author',
+            generateNodeId(AUTHOR, node.primary_author.id)
+        );
+    }
+    if (node.tags) {
+        replacePropWithNodeRef(
+            node,
+            'tags',
+            tags => tags.map(t => generateNodeId(TAG, t.id))
+        );
+    }
+    if (node.authors) {
+        replacePropWithNodeRef(
+            node,
+            'authors',
+            authors => authors.map(a => generateNodeId(AUTHOR, a.id))
+        );
+    }
+    return node;
+});
+
+module.exports.PageNode = createNodeFactory(PAGE, (node) => {
+    if (node.primary_author) {
+        replacePropWithNodeRef(
+            node,
+            'primary_author',
+            generateNodeId(AUTHOR, node.primary_author.id)
+        );
+    }
+    if (node.tags) {
+        replacePropWithNodeRef(
+            node,
+            'tags',
+            tags => tags.map(t => generateNodeId(TAG, t.id))
+        );
+    }
+    if (node.authors) {
+        replacePropWithNodeRef(
+            node,
+            'authors',
+            authors => authors.map(a => generateNodeId(AUTHOR, a.id))
+        );
+    }
+    return node;
+});
+
 module.exports.TagNode = createNodeFactory(TAG);
 module.exports.AuthorNode = createNodeFactory(AUTHOR);
