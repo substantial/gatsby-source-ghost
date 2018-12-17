c5c9f434baac84c6c442cb179924b003e170d6af
Beau Dacious <dacious.beau@gmail.com>
2018-12-16 19:02:27 -0800

    update main test so that it passes

--- test/gatsby-node.test.js
+++ test/gatsby-node.test.js
@@ -26,8 +26,18 @@ describe('Basic Functionality ', function () {
             {slug: 'about', page: true}
         ]);
 
+        sinon.stub(GhostAPI, 'fetchAllTags').resolves([
+            {slug: 'getting-started'},
+            {slug: 'hash-feature-img'}
+        ]);
+
+        sinon.stub(GhostAPI, 'fetchAllUsers').resolves([
+            {name: 'Ghost Writer'},
+            {name: 'Ghost Author'}
+        ]);
+
         gatsbyNode
-            .sourceNodes({boundActionCreators: {createNode}}, {})
+            .sourceNodes({actions: {createNode}}, {})
             .then(() => {
                 createNode.callCount.should.eql(6);
 
@@ -35,11 +45,11 @@ describe('Basic Functionality ', function () {
 
                 // Check that we get the right type of node created
                 getArg(0).internal.should.have.property('type', 'GhostPost');
-                getArg(1).internal.should.have.property('type', 'GhostTag');
+                getArg(1).internal.should.have.property('type', 'GhostPage');
                 getArg(2).internal.should.have.property('type', 'GhostTag');
-                getArg(3).internal.should.have.property('type', 'GhostAuthor');
+                getArg(3).internal.should.have.property('type', 'GhostTag');
                 getArg(4).internal.should.have.property('type', 'GhostAuthor');
-                getArg(5).internal.should.have.property('type', 'GhostPage');
+                getArg(5).internal.should.have.property('type', 'GhostAuthor');
 
                 done();
             })
